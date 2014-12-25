46216e786e713998c81eca7ceb80ab85a353cb08
Emily Siow <emily.siow@gmail.com>
2014-12-25 18:08:02 +1100

    Test welcome feature

---
+++ test/com/twu/biblioteca/WelcomeTest.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class WelcomeTest {
+
+
+    @Test
+    public void testWelcomeMessage() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        BibliotecaApp.main(new String[] {});
+        assertEquals("Welcome to Biblioteca!\n", outContent.toString());
+    }
+}
