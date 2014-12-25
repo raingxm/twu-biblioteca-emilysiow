5681db0f4d46c9ffa6c47ab3d370634863a4e10e
Emily Siow <emily.siow@gmail.com>
2014-12-25 19:54:17 +1100

    Displays welcome message and book list on startup

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,21 @@
 package com.twu.biblioteca;
+import java.util.List;
+import java.util.ArrayList;
 
 public class BibliotecaApp {
 
+    private static List<String> bookList = new ArrayList<String>();
+
     public static void main(String[] args) {
+        bookList.add("Test-Driven Development By Example");
+        bookList.add("The Agile Samurai");
+        bookList.add("Head First Java");
+        bookList.add("Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability");
+
         System.out.println("Welcome to Biblioteca!");
+        System.out.println("Book list:");
+        for (String book : bookList) {
+            System.out.println(book);
+        }
     }
 }
