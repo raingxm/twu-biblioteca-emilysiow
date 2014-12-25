5e5e7f6a375e4f6f6cb71d93349196cd81db6148
Emily Siow <emily.siow@gmail.com>
2014-12-25 19:48:38 +1100

    Test display welcome and book list on startup

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -5,16 +5,31 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.lang.StringBuilder;
+import java.util.List;
+import java.util.ArrayList;
 
 public class BibliotecaAppTest {
 
-
     @Test
-    public void testWelcomeMessage() {
+    public void testBibliotecaStartup() {
+        List<String> bookList = new ArrayList<String>();
+        bookList.add("Test-Driven Development By Example");
+        bookList.add("The Agile Samurai");
+        bookList.add("Head First Java");
+        bookList.add("Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability");
+
+        StringBuilder startupOutput = new StringBuilder();
+        startupOutput.append("Welcome to Biblioteca!\n");
+        startupOutput.append("Book list:\n");
+        for (String book : bookList) {
+            startupOutput.append(book + "\n");
+        }
+
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-
         BibliotecaApp.main(new String[] {});
-        assertEquals("Welcome to Biblioteca!\n", outContent.toString());
+
+        assertEquals(startupOutput.toString(), outContent.toString());
     }
 }
