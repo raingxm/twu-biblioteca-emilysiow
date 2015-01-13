48594ab561546eb4d8a6eb361f0749fd28eb8e89
Emily Siow <emily.siow@gmail.com>
2015-01-14 10:39:32 +1100

    For previous commit

---
+++ test/com/twu/biblioteca/BibliotecaAppTester.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by esiow on 14/01/2015.
+ */
+public class BibliotecaAppTester {
+
+    public void main(String[] args) {
+        BibliotecaApp app = new BibliotecaApp(initBookList());
+        app.run();
+    }
+
+    public static List<Book> initBookList() {
+        List<Book> bookList = new ArrayList<Book>();
+        bookList.add(createNewBook("Test-Driven Development By Example", "Kent Beck", 2003));
+        bookList.add(createNewBook("The Agile Samurai", "Jonathan Rasmusson", 2010));
+        bookList.add(createNewBook("Head First Java", "Kathy Sierra & Bert Bates", 2005));
+        bookList.add(createNewBook("Don't Make Me Think, Revisited", "Steve Krug", 2014));
+        return bookList;
+    }
+
+    static Book createNewBook(String title, String author, int yearPublished) {
+        return new Book(title, author, yearPublished);
+    }
+}
